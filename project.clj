9511db1ca8c105e2d479172e1fa3f54adc6443b4
Yu Shen <yubrshen@gmail.com>
2015-07-01 16:50:59 +0800

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+
+(defproject multiplication-table-of-prime "0.1.0-SNAPSHOT"
+  :description "This provides multiplication table of prime numbers"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot multiplication-table-of-prime.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]}})
